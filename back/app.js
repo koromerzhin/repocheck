6b5ad6e5f02ee0245553e72b6cc823b3f06f65d0
koromerzhin <308012+koromerzhin@users.noreply.github.com>
2021-02-18 14:10:30 +0100

    feat: connexion front / back

--- back/app.js
+++ back/app.js
@@ -3,11 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var repositoriesRouter = require('./routes/repositories');
 
 var app = express();
+var corsOptions = {
+  origin: 'http://front-repocheck.traefik.me',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
